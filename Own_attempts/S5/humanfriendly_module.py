1842ad2100dc5f6c6f79fc85d98a439d071ae93c
donkyjohn <benjamin.hoeree@student.howest.be>
2023-12-27 21:39:47 +0100

    test

--- Own_attempts/S5/humanfriendly_module.py
+++ Own_attempts/S5/humanfriendly_module.py
@@ -3,3 +3,5 @@
 import humanfriendly as hf
 
 print("\nSize of file in bytes: ", hf.format_size(123456789, binary=True))
+
+#############################################################################################################
