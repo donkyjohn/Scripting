aeb41aaab2abcce27bf04d90479535dbe0b8be64
donkyjohn <benjamin.hoeree@student.howest.be>
2023-12-28 01:17:48 +0100

    chapter5CSV

---
+++ Own_attempts/S5/CSV_example.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import csv
+
+with open('/home/guest/_repos/Scripting/S5_Filesystem_reading_writing_files/urine.csv') as csv.file:
+    csv_reader = csv.reader(csv.file, delimiter=',')
+    count = 0
+    for row in csv_reader:
+        if count == 0:
+            print("Column names are:")
+            print("{0:5s} | {1:5s} | {2:10s}".format(row[0], row[1], row[2]))
+            count += 1
+        elif(count <= 20):
+            print("{0:5s} | {1:5s} | {2:10s}".format(row[0], row[1], row[2]))
+            count += 1
+    print("Processed {} lines.".format(count))
+    csv.file.close()      
+
+with open('/home/guest/_repos/Scripting/S5_Filesystem_reading_writing_files/urine.csv', mode='r') as csvfile2:
+    csvreader = csv.DictReader(csvfile2)
+    count2 = 0  
+    for row in csvreader:
+        if(count<=5):
+            print("{0:5s} | {1:5s} | {2:10s}".format(row[""], row["r"], row["gravity"]))
+            count2 += 1
+    print("Processed {} lines.".format(count2))
+csvfile2.close()
+
+with open('/home/guest/_repos/Scripting/Own_attempts/S5/pers.csv', mode='w') as pers_file:
+    pers_writer = csv.writer(pers_file, 
+    delimiter=';', # default is ',' this is the character used to separate fields.
+    quotechar='"', # default is '"' this is the character used to quote fields containing special characters.
+    quoting=csv.QUOTE_MINIMAL) # default is QUOTE_MINIMAL this specifies the character used to escape the delimiter if quoting is set to QUOTE_NONE.
+    pers_writer.writerow(['John Snow', '1,73', '32'])
+    pers_writer.writerow(['Arya Stark', '1,55', '21'])
+    pers_writer.writerow(['Daenerys Targaryen', '1,57', '32'])
+pers_file.close()
--- Own_attempts/S5/CSV_files.py
+++ Own_attempts/S5/CSV_files.py
@@ -14,3 +14,15 @@ csv.reader('source') # returns a reader object which iterates over lines in the 
 # dialect = 'dialect' # specifies the dialect of the source.
 
 csv.writer('destination') # returns a writer object which converts the user's data into delimited strings on the given destination.
+
+# options
+# csv.QUOTE_MINIMAL # default value, quotes fields containing special characters.
+# csv.QUOTE_ALL # quotes all fields.
+# csv.QUOTE_NONNUMERIC # quotes all non-numeric fields.
+# csv.QUOTE_NONE # does not quote fields containing special characters.
+
+csv.register_dialect('myDialect',
+                    delimiter = '|', # this is the character used to separate fields.
+                    quoting = csv.QUOTE_ALL, # this specifies the character used to escape the delimiter if quoting is set to QUOTE_NONE.
+                    skipinitialspace = True) # this specifies how to interpret whitespace which immediately follows a delimiter.
+# this last piece of code registers a dialect with the name 'myDialect' and the specified parameters.
